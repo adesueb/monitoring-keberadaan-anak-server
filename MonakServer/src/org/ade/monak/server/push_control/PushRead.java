ac42fe74f057e43855ddc80daa13e50b43069ce3
ade <ade@ade>
2013-02-24 16:12:17 +0700

    step 2 ping pong

--- MonakServer/src/org/ade/monak/server/push_control/PushRead.java
+++ MonakServer/src/org/ade/monak/server/push_control/PushRead.java
@@ -2,7 +2,8 @@ package org.ade.monak.server.push_control;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Queue;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.ade.monak.server.util.Handler;
 
@@ -21,25 +22,46 @@ public class PushRead implements Runnable{
 		start = false;
 	}
 	
+	public void addHandler(String key, Handler handler){
+		if(mapHandler!=null){
+			mapHandler = new HashMap<String, Handler>();
+		}
+		mapHandler.put(key, handler);
+	}
+	
+	public void removeHandler(String key){
+		if(mapHandler!=null){
+			mapHandler.remove(key);
+		}
+	}
+	
 	@Override
 	public void run() {
 		while(start){
 			try {
 				String pesan = buff.readLine();
 				if(pesan!=PONG){
-					Handler handler = queue.poll();
-					if(handler!=null){
-						handler.sendMessage(pesan);
+					String [] pesanSplit = pesan.split("#");
+					if(pesanSplit.length>0){
+						String key = pesanSplit[0];
+						if(key!=null && key.equals("") && mapHandler!=null){
+							Handler handler = mapHandler.get(key);
+							if(handler!=null){
+								handler.sendMessage(pesan);
+								removeHandler(key);
+							}	
+						}		
 					}
 				}
 			}catch (IOException e) {
+				stop();
 				e.printStackTrace();
 			}
 			
 		}
 	}
 	
-	private Queue<Handler> queue;
+	private Map<String,Handler> mapHandler;
 	private boolean start = false;
 	private final BufferedReader buff;
 	private final static String PONG = "Y";
